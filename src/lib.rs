5721d76d1dcb1a2cecf43ba4a9076b5aaf7b1dd5
Sergei Surovtsev <ssurovsev@gmail.com>
2023-10-31 18:41:58 +0300

    VeilidApp resouce ref

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,14 @@ use bevy::prelude::*;
 use bevy_tokio_tasks::*;
 use veilid_duplex::veilid::P2PApp;
 
-#[derive(Resource, Deref, DerefMut)]
-struct VeilidApp(P2PApp);
+#[derive(Resource, Deref, DerefMut, Default)]
+pub struct VeilidApp(Option<P2PApp>);
 
 pub struct VeilidPlugin;
 impl Plugin for VeilidPlugin {
     fn build(&self, app: &mut App) {
         // app.insert_resource(veilid_app);
+        app.init_resource::<VeilidApp>();
         app.add_systems(Startup, initialize_veilid_app);
         app.add_event::<VeilidInitializedEvent>();
     }
@@ -19,13 +20,15 @@ impl Plugin for VeilidPlugin {
 fn initialize_veilid_app(runtime: ResMut<TokioTasksRuntime>) {
     runtime.spawn_background_task(|mut ctx| async move {
         let veilid_app = P2PApp::new().await;
-
-        // ew_veilid_initialized.send(VeilidInitializedEvent);
+        if veilid_app.is_err() {
+            return;
+        }
+        let veilid_app = veilid_app.unwrap();
 
         ctx.run_on_main_thread(move |ctx| {
             let world = ctx.world;
 
-            let veilid_app = VeilidApp(veilid_app.unwrap());
+            let veilid_app = VeilidApp(Some(veilid_app));
             world.insert_resource(veilid_app);
 
             world.send_event(VeilidInitializedEvent);
